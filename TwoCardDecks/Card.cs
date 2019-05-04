8b5941d3bb9aee01d4acbee11506b90c523348eb
Mark <Mark@DESKTOP-16DN5B4>
2019-05-04 19:07:50 +0200

    First commit - card class with enums values suits

---
+++ TwoCardDecks/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoCardDecks
+{
+    public class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return $"{Value.ToString()} of {Suit.ToString()}"; 
+            }
+        }
+    }
+}
